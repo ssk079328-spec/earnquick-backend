f1fba5dca47174ff4bcab1a7c73157032802fafb
ssk079328-spec <ssk079328@gmail.com>
2026-02-15 20:11:05 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
-const { Pool } = require("pg");
-require("dotenv").config();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-module.exports = pool;
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  telegram_id BIGINT UNIQUE,
+  name TEXT,
+  points INT DEFAULT 0
+);
