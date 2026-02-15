e7d04bb3b73bf3d5e6153cb20b03841137d0e2df
ssk079328-spec <ssk079328@gmail.com>
2026-02-15 22:02:19 +0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,14 @@
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  telegram_id BIGINT UNIQUE,
-  full_name TEXT,
-  ref_code TEXT,
-  points INTEGER DEFAULT 0
+    telegram_id BIGINT PRIMARY KEY,
+    name TEXT,
+    points INTEGER DEFAULT 0
+);
+
+CREATE TABLE withdrawals (
+    id SERIAL PRIMARY KEY,
+    telegram_id BIGINT,
+    amount INTEGER,
+    number TEXT,
+    status TEXT DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
